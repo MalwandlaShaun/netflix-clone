e864d0d1edec3c4dbd4b8b472ef7b38ab78a58ee
Malwandla Hlungwani <hlungwanimalwandlashaun@gmail.com>
2022-06-28 19:33:42 +0200

    Updates

---
+++ precache-manifest.00b2cf0f2e9935ffb740cc49b9bd47fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e2059681bd8c25d3e29712bd090c25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79960543ca4319493eb7",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "94dceb35dfea8e855660",
+    "url": "/static/css/main.efe27cce.chunk.css"
+  },
+  {
+    "revision": "79960543ca4319493eb7",
+    "url": "/static/js/2.a290d67a.chunk.js"
+  },
+  {
+    "revision": "c9fe8c12d96a6fc710646af11952fd5e",
+    "url": "/static/js/2.a290d67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dceb35dfea8e855660",
+    "url": "/static/js/main.7aadca1a.chunk.js"
+  },
+  {
+    "revision": "d6ecf84704e8f0ba8d31",
+    "url": "/static/js/runtime-main.d97b9143.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  },
+  {
+    "revision": "85597ea9bc9d29c3c451cd11509c15d7",
+    "url": "/static/media/red-x.85597ea9.svg"
+  }
+]);
